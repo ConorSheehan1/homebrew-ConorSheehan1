a36100c295e2bd6ec67f6a5e42654e6b7e37510e
Conor Sheehan <conor.sheehan.dev@gmail.com>
2020-11-30 22:16:06 +0000

    fix bundler version for ruby 2.7.
    add lint step to ci.
    brew uninstall before install.

--- spec/osxdocker_spec.rb
+++ spec/osxdocker_spec.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 # TODO: find a way to test local files rather than only files pushed to github
 # TODO: make generic test that loops over files ../*.rb, creates test per file?
 describe "osxdocker" do
+  # ensure fresh install
+  before do
+    system %(brew uninstall conorsheehan1/conorsheehan1/osxdocker)
+  end
   it "should not throw an error when brew installing" do
     # TODO: use raw github url to install specific version
     # https://apple.stackexchange.com/questions/282780/how-do-i-install-a-specific-release-of-a-homebrew-formula-on-github
