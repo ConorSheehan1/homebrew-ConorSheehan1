d910b4dc2b46cf7f6c73face07050be9d16c6355
Conor Sheehan <conor.sheehan.dev@gmail.com>
2020-11-30 22:05:45 +0000

    add basic test, rubocop etc.
    fix description of osxdocker

--- README.md
+++ README.md
@@ -2,12 +2,20 @@ A [homebrew](https://docs.brew.sh/) tap for things I've made.
 
 # Example
 ```bash
-brew install ConorSheehan1/ConorSheehan1/osxdocker
+brew install conorsheehan1/conorsheehan1/osxdocker
 ```
 
 # dev
 ### python
 Formula scaffold using [poet](https://github.com/tdsmith/homebrew-pypi-poet)
 ```bash
+poetry install
 poetry run poet -f osxdocker
+```
+
+### ruby
+```bash
+bundle install
+bundle exec rubocop # --safe-auto-correct
+bundle exec rspec
 ```
---
+++ spec/osxdocker_spec.rb
@@ -0,0 +1,11 @@
+# TODO: find a way to test local files rather than only files pushed to github
+# TODO: make generic test that loops over files ../*.rb, creates test per file?
+describe "osxdocker" do
+  it "should not throw an error when brew installing" do
+    # TODO: use raw github url to install specific version
+    # https://apple.stackexchange.com/questions/282780/how-do-i-install-a-specific-release-of-a-homebrew-formula-on-github
+    expect do
+      system %(brew install conorsheehan1/conorsheehan1/osxdocker)
+    end.not_to(raise_error)
+  end
+end
